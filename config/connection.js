77840fc847260b510c967e34c2d2db1b9acbeab6
Rafael Barbosa <rafael.barbosa51@gmail.com>
2023-03-17 16:11:34 -0500

    Create User Model In this commit, I created the user model. This includes the friendCount virtual

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const { connect, connection } = require('mongoose');
 
-//127.0.0.1:27017
+
 connect('mongodb://127.0.0.1:27017/EliteSocial', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
---
+++ models/User.js
@@ -0,0 +1,53 @@
+const {Schema, model} = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: Schema.Types.String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: Schema.Types.String,
+            required: true,
+            unique: true,
+            validate: {
+                validator: (v) => {
+                    return /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/.test(v);
+                },
+                message: p => `${p.value} is not a valid email address!`
+            }
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        }
+    }
+);
+userSchema.virtual('friendCount').get( function() {
+    return this.friends.length;
+});
+const User = model('user', userSchema);
+module.exports = User;
+/*
+videos: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Video',
+      },
+    ],
+*/
