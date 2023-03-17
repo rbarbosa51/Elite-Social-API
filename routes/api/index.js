10145bf23cb636216920e9aa6e1c6186580b95e1
Rafael Barbosa <rafael.barbosa51@gmail.com>
2023-03-17 18:01:24 -0500

    Created Routes file structures In this commit created the file structure with boilerplate code for the routes

---
+++ routes/api/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const userRoutes = require('./userRoutes.js');
+const thoughtsRoutes = require('./thoughtsRoutes.js')
+
+router.use('/users', userRoutes);
+router.use('/thoughts', thoughtsRoutes)
+
+module.exports = router;
